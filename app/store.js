409239ec798bd2b7573dda13c2d4bc734fca46b5
svend <sven.depickere@student.howest.be>
2019-06-14 17:48:09 +0200

    updated added localstorage

--- app/store.js
+++ app/store.js
@@ -1,5 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import localStorage from 'nativescript-localstorage';
+
+const NSVuexPersistent = store => {
+    let storageStr = localStorage.getItem('ns-vuex-persistent');
+    if (storageStr) {
+        store.replaceState(JSON.parse(storageStr))
+    }
+    store.subscribe((mutation, state) => {
+        // Suscribe hook.
+        localStorage.setItem('ns-vuex-persistent', JSON.stringify(state));
+    })
+};
 
 Vue.use(Vuex);
 
@@ -7,6 +19,7 @@ export default new Vuex.Store({
     state: {
         language: null
     },
+    plugins: [NSVuexPersistent],
     mutations: {
         updateLanguage(state, newLanguage) {
             state.language = newLanguage;
