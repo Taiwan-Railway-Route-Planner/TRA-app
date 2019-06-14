2c86bf43fdd730a3aaeafeff655fb74cf336c986
svend <sven.depickere@student.howest.be>
2019-06-14 16:53:44 +0200

    init language difference

---
+++ app/components/Route/enRoute.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "enRoute"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/components/Select/enStationSelect.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "enStationSelect"
+    }
+</script>
+
+<style scoped>
+
+</style>
