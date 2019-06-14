2ed7a5cdc84199cd01abb9064ffd82791b6f8001
svend <sven.depickere@student.howest.be>
2019-06-14 11:45:12 +0200

    Added errormessage

---
+++ app/assets/js/request/errorMessage.js
@@ -0,0 +1,22 @@
+/**
+ Created by svend on 14/06/2019.
+ **/
+
+export default (function () {
+
+    const messages = {
+        eng: {
+            stations: "Couldn't get the station information",
+            route: "The route is currently not available"
+        },
+        zh: {
+            stations: "Couldn't get the station information",
+            route: "The route is currently not available"
+        }
+    };
+
+    return {
+        messages
+    }
+
+})();
