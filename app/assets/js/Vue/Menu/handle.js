8fb4d40a982c06f0722e358a961ac4184ef2e7b4
svend <sven.depickere@student.howest.be>
2019-06-14 16:53:12 +0200

    Test

--- app/assets/js/Vue/Menu/handle.js
+++ app/assets/js/Vue/Menu/handle.js
@@ -8,7 +8,7 @@ export default (function () {
 
     async function getAllPossibleStations(_self) {
         const stations = await requestHandler.request.stations(_self);
-        return stations;
+        return stations.data;
     }
 
 
