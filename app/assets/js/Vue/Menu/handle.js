c694fe44575e2ed8aa2f1de7d5ddab2548df4f65
svend <sven.depickere@student.howest.be>
2019-06-14 13:01:49 +0200

    Handle request builder

---
+++ app/assets/js/Vue/Menu/handle.js
@@ -0,0 +1,19 @@
+/**
+ Created by svend on 14/06/2019.
+ **/
+
+const requestHandler = require("../../request/requestHandler");
+
+export default (function () {
+
+    async function getAllPossibleStations(_self) {
+        const stations = await requestHandler.request.stations(_self);
+        return stations;
+    }
+
+
+    return {
+        getAllPossibleStations
+    }
+
+})();
