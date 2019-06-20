78eb515758fc5c5a9de0c668fd27ef3abded5482
svend <sven.depickere@student.howest.be>
2019-06-20 13:20:34 +0200

    Do set up for station select

--- app/assets/js/Vue/Select/handle.js
+++ app/assets/js/Vue/Select/handle.js
@@ -2,18 +2,36 @@
  Created by svend on 14/06/2019.
  **/
 
-const requestHandler = require("../../request/requestHandler");
+const getStationDetails = require("./getStationDetails");
+const language = require("./language");
 
 export default (function () {
 
-    async function getAllPossibleStations(_self) {
-        const stations = await requestHandler.request.stations(_self);
-        return stations.data;
+
+    const setUpSelectVue = async function (_self) {
+        loadLanguage(_self);
+        setHintText(_self);
+        await loadStationDetails(_self);
+    };
+
+    function loadLanguage(_self) {
+        if (_self.$store.state.language === 'ZH'){
+            _self.data = language.language.zh;
+        } else {
+            _self.data = language.language.en;
+        }
     }
 
+    function setHintText(_self){
+        _self.data.searchBar.hintText.now = _self.data.searchBar.hintText.startStation;
+    }
+
+    async function loadStationDetails(_self) {
+        await getStationDetails.getAllPossibleStations(_self);
+    }
 
     return {
-        getAllPossibleStations
+        setUpSelectVue
     }
 
 })();
