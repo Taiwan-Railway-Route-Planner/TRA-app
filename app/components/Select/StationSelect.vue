a840274d136933bd1401d22f594460afb285a6cd
svend <sven.depickere@student.howest.be>
2019-06-20 13:21:10 +0200

    uniform station select

--- app/components/Select/StationSelect.vue
+++ app/components/Select/StationSelect.vue
@@ -1,19 +1,54 @@
 <template>
-    <Page actionbarhidden="true">
-        <en-station-select v-if="$store.state.language === 'EN'"></en-station-select>
-        <zh-station-select v-else></zh-station-select>
+    <Page actionBarHidden="true">
+<!--        <en-station-select v-if="$store.state.language === 'EN'"></en-station-select>-->
+<!--        <zh-station-select v-else></zh-station-select>-->
+        <DockLayout>
+            <FlexboxLayout dock="center" class="searchBar">
+                <SearchBar :hint="data.searchBar.hintText.now" :text="data.searchBar.search" @textChange="onTextChanged" @submit="onSubmit"></SearchBar>
+            </FlexboxLayout>
+        </DockLayout>
     </Page>
 </template>
 
 <script>
-    import enStationSelect from "./enStationSelect"
-    import zhStationSelect from "/zhStationSelect"
+    // import enStationSelect from "./enStationSelect"
+    // import zhStationSelect from "./zhStationSelect"
+
+    // export default {
+    //     created() {
+    //        console.log(this.$store.state.language)
+    //     },
+    //     components: {zhStationSelect, enStationSelect}
+    // }
+
+
+    import handle from "../../assets/js/Vue/Select/handle"
 
     export default {
-        components: {zhStationSelect, enStationSelect}
+        async created(){
+            await handle.setUpSelectVue(this);
+        },
+        data() {
+            return {
+                data: null
+            }
+        },
+        methods: {
+            onTextChanged: function () {
+
+            },
+            onSubmit: function () {
+
+            }
+        }
     }
 </script>
 
 <style scoped>
+
+    .searchBar{
+        width: 100%;
+    }
+
 
 </style>
