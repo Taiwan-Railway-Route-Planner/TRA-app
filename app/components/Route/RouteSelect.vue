b43b66561f10f36510d33f6678de0d5f03c726e1
svend <sven.depickere@student.howest.be>
2019-06-20 10:41:15 +0200

    Made main components to switch between languages

---
+++ app/components/Route/RouteSelect.vue
@@ -0,0 +1,19 @@
+<template>
+    <Page actionbarhidden="true">
+        <enRoute v-if="$store.state.language === 'EN'"></enRoute>
+        <zhRoute v-else></zhRoute>
+    </Page>
+</template>
+
+<script>
+    import enRoute from "./enRoute"
+    import zhRoute from "./zhRoute"
+
+    export default {
+        components: {enRoute, zhRoute}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/components/Select/StationSelect.vue
@@ -0,0 +1,19 @@
+<template>
+    <Page actionbarhidden="true">
+        <en-station-select v-if="$store.state.language === 'EN'"></en-station-select>
+        <zh-station-select v-else></zh-station-select>
+    </Page>
+</template>
+
+<script>
+    import enStationSelect from "./enStationSelect"
+    import zhStationSelect from "/zhStationSelect"
+
+    export default {
+        components: {zhStationSelect, enStationSelect}
+    }
+</script>
+
+<style scoped>
+
+</style>
