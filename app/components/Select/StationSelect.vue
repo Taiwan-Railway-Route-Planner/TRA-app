88f72569391d6bad9623d8efbe24d4d9bdf12044
svend <sven.depickere@student.howest.be>
2019-06-20 13:28:45 +0200

    Deleted unneeded code

--- app/components/Select/StationSelect.vue
+++ app/components/Select/StationSelect.vue
@@ -1,8 +1,10 @@
 <template>
     <Page actionBarHidden="true">
-<!--        <en-station-select v-if="$store.state.language === 'EN'"></en-station-select>-->
-<!--        <zh-station-select v-else></zh-station-select>-->
+
         <DockLayout>
+            <FlexboxLayout dock="top" class="topBar">
+                <Label class="fas" :text="'\uf013' | unescape"></Label>
+            </FlexboxLayout>
             <FlexboxLayout dock="center" class="searchBar">
                 <SearchBar :hint="data.searchBar.hintText.now" :text="data.searchBar.search" @textChange="onTextChanged" @submit="onSubmit"></SearchBar>
             </FlexboxLayout>
@@ -11,17 +13,6 @@
 </template>
 
 <script>
-    // import enStationSelect from "./enStationSelect"
-    // import zhStationSelect from "./zhStationSelect"
-
-    // export default {
-    //     created() {
-    //        console.log(this.$store.state.language)
-    //     },
-    //     components: {zhStationSelect, enStationSelect}
-    // }
-
-
     import handle from "../../assets/js/Vue/Select/handle"
 
     export default {
