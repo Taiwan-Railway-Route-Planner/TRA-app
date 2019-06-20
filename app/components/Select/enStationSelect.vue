6a43d45dcccb832f714dfef57930926cf7f992d9
svend <sven.depickere@student.howest.be>
2019-06-20 13:28:27 +0200

    Update enStationSelect.vue

--- app/components/Select/enStationSelect.vue
+++ app/components/Select/enStationSelect.vue
@@ -1,6 +1,8 @@
 <template>
     <Page actionBarHidden="true">
-
+        <DockLayout dock="center">
+            <SearchBar :hint="searchBar.hintText.now" :text="searchBar.search" @textChange="onTextChanged" @submit="onSubmit"></SearchBar>
+        </DockLayout>
     </Page>
 </template>
 
@@ -8,7 +10,36 @@
     import handle from "../../assets/js/Vue/Select/handle"
 
     export default {
-        name: "enStationSelect"
+        created(){
+            console.log("test");
+            this.searchBar.hintText.now = this.searchBar.hintText.startStation;
+            console.log("456");
+        },
+        data() {
+            return {
+                stationInfo: null,
+                searchBar: {
+                    hintText: {
+                        now: '',
+                        startStation:  'Choose your departure station',
+                        endStation: 'Choose your arrival station'
+                    },
+                    search: null
+                },
+                routeDetails: {
+                    departure: {},
+                    arrival: {}
+                }
+            }
+        },
+        methods: {
+            onTextChanged: function () {
+
+            },
+            onSubmit: function () {
+
+            }
+        }
     }
 </script>
 
