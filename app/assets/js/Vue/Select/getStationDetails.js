7274a2440349ef100112e747c9d527d9169e1912
svend <sven.depickere@student.howest.be>
2019-06-22 12:20:07 +0200

    Fix issue when undefined + save to other var

--- app/assets/js/Vue/Select/getStationDetails.js
+++ app/assets/js/Vue/Select/getStationDetails.js
@@ -7,13 +7,14 @@ const requestHandler = require("../../request/requestHandler");
 module.exports = (function () {
 
     async function getAllPossibleStations(_self) {
-        if (_self.$store.state.stations === null){
+        if (_self.$store.state.stations === null || _self.$store.state.stations === undefined){
             const stations = await requestHandler.request.stations(_self);
             _self.$store.commit('updateStation', stations.data);
             _self.data.stationInfo = stations.data;
         } else {
             _self.data.stationInfo = _self.$store.state.stations;
         }
+        _self.filteredStations = _self.data.stationInfo.stations;
     }
 
     return {
