0dd87ff1c83ec652845f13f75fc237d6a33cf9e0
svend <sven.depickere@student.howest.be>
2019-06-25 10:47:36 +0200

    Create dateModal.vue

---
+++ app/components/modals/dateModal.vue
@@ -0,0 +1,49 @@
+<template>
+    <DockLayout class="modal">
+        <FlexboxLayout dock="bottom" class="confirmOrDiscard">
+            <Label class="btn discardBtn" @tap="discard" :text="$props.time.modal.bottom.leftBtn"></Label>
+            <Label class="btn confirmBtn" @tap="confirm" :text="$props.time.modal.bottom.rightBtn"></Label>
+        </FlexboxLayout>
+        <FlexboxLayout dock="center">
+            <DatePicker v-model="$props.timeModal.selectedDate" @dateChange="dateChange" :minDate="$props.timeModal.minDate"></DatePicker>
+
+        </FlexboxLayout>
+
+    </DockLayout>
+</template>
+
+<script>
+    
+    import moment from "moment"
+    
+    export default {
+        props: ['time', 'timeModal'],
+        methods: {
+            dateChange: function () {
+                console.log("dkfhsdkfhsd");
+                console.log('this.selectedDate', this.$props.timeModal.selectedDate);
+            },
+            discard: function () {
+                this.$modal.close();
+            },
+            confirm: function () {
+                this.$props.time.modal.center.date.actual = moment(this.$props.timeModal.selectedDate).format('llll').replace(moment().format('LT'), '').replace('一', '').replace('00:00', '');
+                this.$modal.close();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .modal {
+        backgroud-color: white;
+        height: 350;
+        width: 250;
+        text-align: left;
+        color: #000;
+    }
+
+
+
+</style>
