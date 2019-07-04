957bd501c039bfd040f77887b6604180fec628b8
svend <sven.depickere@student.howest.be>
2019-07-04 13:49:25 +0200

    Update errorMessage.js for new request

--- app/assets/js/request/errorMessage.js
+++ app/assets/js/request/errorMessage.js
@@ -7,11 +7,13 @@ module.exports = (function () {
     const messages = {
         eng: {
             stations: "Couldn't get the station information",
-            route: "The route is currently not available"
+            route: "The route is currently not available",
+            date: "No routes available for that date."
         },
         zh: {
             stations: "Couldn't get the station information",
-            route: "The route is currently not available"
+            route: "The route is currently not available",
+            date: "No routes available for that date."
         }
     };
 
