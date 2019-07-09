6df6c3e8019093dde2e68c93f2f11c98b47a7b01
svend <sven.depickere@student.howest.be>
2019-07-09 14:42:02 +0200

    Update getRoutesOfADay.js

--- app/assets/js/Vue/Route/getRoutesOfADay.js
+++ app/assets/js/Vue/Route/getRoutesOfADay.js
@@ -3,11 +3,18 @@
  **/
 
 const requestHandler = require("../../request/requestHandler");
+const moment = require('moment');
 
 module.exports = (function () {
 
     async function getAllRoutesOfACertainDay(_self) {
+        // setLongDate(_self);
         return await requestHandler.request.date(_self);
+    }
+
+    function setLongDate(_self) {
+        _self.longDate = _self.$props.routeDetails.time.date.real;
+        console.log(_self.longDate);
     }
 
 
