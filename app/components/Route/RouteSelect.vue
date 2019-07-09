cf75d8667932ff0158d299b4aa2c9df5ca2903ff
svend <sven.depickere@student.howest.be>
2019-07-09 14:42:12 +0200

    Added test data

--- app/components/Route/RouteSelect.vue
+++ app/components/Route/RouteSelect.vue
@@ -1,17 +1,102 @@
 <template>
-    <Page actionbarhidden="true">
-        <enRoute v-if="$store.state.language === 'EN'"></enRoute>
-        <zhRoute v-else></zhRoute>
+    <Page actionBarHidden="true">
+        <DockLayout>
+            <FlexboxLayout dock="bottom" class="bottom-menu">
+                <FlexboxLayout class="menu-choose" @tap="">
+                    <Label class="fas" :text="'\uf3c5' | unescape" ></Label>
+                </FlexboxLayout>
+                <FlexboxLayout class="menu-choose" @tap="navToStar">
+                    <Label class="fas" :text="'\uf005' | unescape"></Label>
+                </FlexboxLayout>
+                <FlexboxLayout class="menu-choose" @tap="navToSet">
+                    <Label class="fas" :text="'\uf509' | unescape"></Label>
+                </FlexboxLayout>
+            </FlexboxLayout>
+        </DockLayout>
     </Page>
 </template>
 
 <script>
-    import enRoute from "./enRoute"
-    import zhRoute from "./zhRoute"
+
+    import handle from "../../assets/js/Vue/Route/handle"
 
     export default {
-        components: {enRoute, zhRoute}
+        props: ['routeDetails'],
+        created: async function() {
+            await handle.handleIncomingRouteDetails(this);
+        },
+        data() {
+            return {
+                longDate: "20190704",
+                props: {
+                    routeDetails: {
+                        language: 'ZH',
+                        departure: {
+                            details:  {
+                                "時刻表編號": 1025,
+                                "traWebsiteCode": "1210",
+                                "站名": "新竹",
+                                "eng站名": "Hsinchu",
+                                "routeCode": [
+                                    4,
+                                    1
+                                ]
+                            },
+                            hint: '選擇哪一站',
+                            label: '從'
+                        },
+                        arrival: {
+                            details: {
+                                "時刻表編號": 2214,
+                                "traWebsiteCode": "1194",
+                                "站名": "六家",
+                                "eng站名": "Liujia",
+                                "routeCode": [
+                                    4
+                                ]
+                            },
+                            hint: '選擇哪一站',
+                            label: '去'
+                        },
+                        time: {
+                            label: '時間',
+                            hint: '',
+                            date: '',
+                            time: '11:45',
+                            local: 'zh-tw',
+                            modal: {
+                                top: {
+                                    first: 'DEPARTURE',
+                                    second: 'ARRIVAL',
+                                },
+                                center: {
+                                    button: 'Now',
+                                    date: {
+                                        actual: null,
+                                        today: null
+                                    }
+                                },
+                                bottom: {
+                                    leftBtn: 'DISCARD',
+                                    rightBtn: 'CONFIRM',
+                                }
+                            }
+                        },
+                        button: 'SEARCH'
+                    }
+                }
+            }
+        },
+        methods: {
+            navToStar: function () {
+
+            },
+            navToSet: function () {
+
+            }
+        }
     }
+
 </script>
 
 <style scoped>
