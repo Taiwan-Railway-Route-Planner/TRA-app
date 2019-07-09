e0ef906425dc98d132c62fac7cabd194094b71d1
svend <sven.depickere@student.howest.be>
2019-07-09 13:17:30 +0200

    Update URL, repo changed, + added line link

--- app/assets/js/request/urlModule.js
+++ app/assets/js/request/urlModule.js
@@ -5,10 +5,11 @@
 module.exports = (function () {
 
     const HOME_IP = "depickeresven.github.io";
-    const BASE_URL = "https://" + HOME_IP + "/TRA-Information/";
+    const BASE_URL = "https://" + HOME_IP + "/TRATimetableData/";
 
     const url = {
         schedules: BASE_URL + "Schedules/DATE.json",
+        lines: BASE_URL + "Lines/LINENAME.json",
         station: BASE_URL + "stationInfo.json",
     };
 
