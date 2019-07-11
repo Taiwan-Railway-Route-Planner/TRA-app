2d1321b4babcce25ce2621016d6cf69593d2c922
svend <sven.depickere@student.howest.be>
2019-07-11 16:45:57 +0200

    created language for Route

---
+++ app/assets/js/Vue/Route/language.js
@@ -0,0 +1,20 @@
+/**
+ Created by svend on 11/07/2019.
+ **/
+
+module.exports = (function () {
+
+    let language = {
+        en: {
+
+        },
+        zh: {
+
+        }
+    };
+
+    return {
+        language
+    }
+
+})();
