2316455c40af8572743e26c8b2842e459fb175c0
svend <sven.depickere@student.howest.be>
2019-07-11 19:52:10 +0200

    Update RouteSelect.vue

--- app/components/Route/RouteSelect.vue
+++ app/components/Route/RouteSelect.vue
@@ -1,11 +1,27 @@
 <template>
     <Page actionBarHidden="true">
         <DockLayout>
-            <FlexboxLayout dock="top">
-
+            <FlexboxLayout dock="top" class="dock-top">
+                <FlexboxLayout class="top">
+                    <FlexboxLayout class="top-title">
+                        <Label :text="data.top.first"></Label>
+                        <Label :text="data.top.second"></Label>
+                    </FlexboxLayout>
+                    <FlexboxLayout v-if="$store.state.language === 'EN'" class="top-data">
+                        <Label :text="props.routeDetails.arrival.details.eng站名"></Label>
+                        <Label :text="props.routeDetails.arrival.details.eng站名"></Label>
+                    </FlexboxLayout>
+                    <FlexboxLayout v-else class="top-data">
+                        <Label :text="props.routeDetails.arrival.details.站名"></Label>
+                        <Label :text="props.routeDetails.arrival.details.站名"></Label>
+                    </FlexboxLayout>
+                    <FlexboxLayout class="top-time">
+                        <Label :text="props.routeDetails.time.date.show + ' ' + props.routeDetails.time.time"></Label>
+                    </FlexboxLayout>
+                </FlexboxLayout>
             </FlexboxLayout>
             <FlexboxLayout dock="bottom" class="bottom-menu">
-                <FlexboxLayout class="menu-choose" @tap="">
+                <FlexboxLayout class="menu-choose" @tap="navToStart">
                     <Label class="fas" :text="'\uf3c5' | unescape"></Label>
                 </FlexboxLayout>
                 <FlexboxLayout class="menu-choose" @tap="navToStar">
@@ -15,8 +31,27 @@
                     <Label class="fas" :text="'\uf509' | unescape"></Label>
                 </FlexboxLayout>
             </FlexboxLayout>
-            <FlexboxLayout dock="center">
-
+            <FlexboxLayout dock="center" class="center">
+                <ListView for="(item, index) in timeTable" @itemTap="onItemTap">
+                    <v-template>
+                        <FlexboxLayout class="travelDetails">
+                            <FlexboxLayout class="timeDetails">
+                                <Label class="timeStamps" :text="item.TimeInfos[props.routeDetails.departure.details.時刻表編號].DepTime"></Label>
+                                <Label class="fas" :text="'\uf061'| unescape"></Label>
+                                <Label class="timeStamps" :text="item.TimeInfos[props.routeDetails.arrival.details.時刻表編號].ArrTime"></Label>
+                            </FlexboxLayout>
+                            <FlexboxLayout class="travelTime">
+                                <Label class="far" :text="'\uf017' | unescape"></Label>
+                                <Label :text="item.travelTime"></Label>
+                            </FlexboxLayout>
+                            <FlexboxLayout class="typeOfTrain">
+                                <Label class="fas" :text="'\uf238' | unescape"></Label>
+<!--                                <Label :text="data.trainTypes[item.trainType]"></Label>-->
+                                <Label :text="item.Train"></Label>
+                            </FlexboxLayout>
+                        </FlexboxLayout>
+                    </v-template>
+                </ListView>
             </FlexboxLayout>
         </DockLayout>
     </Page>
@@ -34,7 +69,10 @@
         data() {
             return {
                 timeTable: [],
+                indexWithClosestToRealTime: 0,
+                index: 0,
                 longDate: "20190704",
+                data: null,
                 props: {
                     routeDetails: {
                         language: 'ZH',
@@ -69,8 +107,8 @@
                             label: '時間',
                             hint: '',
                             date: {
-                                show: '2019年7月11日',
-                                real: "20190704"
+                                show: '2019年7月12日',
+                                real: "20190712"
                             },
                             time: '11:45',
                             local: 'zh-tw',
@@ -98,11 +136,18 @@
             }
         },
         methods: {
+            navToStart: function () {
+                this.$goto('Select');
+            },
             navToStar: function () {
 
             },
             navToSet: function () {
 
+            },
+            onItemTap: function (event) {
+                console.log(event.index);
+                console.log(event.item);
             }
         }
     }
@@ -110,5 +155,86 @@
 </script>
 
 <style scoped>
+
+    .dock-top{
+        background-color: #1a0dab;
+        color: #fff;
+    }
+
+    .dock-top Label{
+        font-size: 17;
+    }
+
+    .top{
+        flex-direction: row;
+        margin-left: 3%;
+        margin-top: 2%;
+    }
+
+    .top .top-title{
+        flex-direction: column;
+        margin-bottom: 1%;
+        margin-right: 5%;
+    }
+
+    .top .top-data{
+        flex-direction: column;
+    }
+
+    .top .top-time{
+        margin-left: 1%;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .center{
+        background-color: #fff;
+        color: #1a0dab;
+    }
+
+    .center .travelDetails{
+        padding-top: 15%;
+        padding-left: 15%;
+        flex-direction: column;
+    }
+
+    .center .travelDetails .timeDetails{
+        padding-bottom: 15%;
+    }
+
+    .center .travelDetails .timeDetails .timeStamps{
+        font-size: 20;
+    }
+
+    .center .travelDetails .timeDetails .fas{
+        margin-left: 10%;
+        margin-right: 10%;
+    }
+
+    .center .travelDetails .travelTime{
+        padding-bottom: 15%;
+    }
+
+    .center .travelDetails .travelTime .far{
+        margin-right: 5%;
+        font-size: 24;
+    }
+
+    .center .travelDetails .travelTime Label{
+        font-size: 18;
+    }
+
+    .center .travelDetails .typeOfTrain{
+        padding-bottom: 20%;
+    }
+
+    .center .travelDetails .typeOfTrain .fas{
+        margin-right: 5%;
+        font-size: 24;
+    }
+
+    .center .travelDetails .typeOfTrain Label{
+        font-size: 18;
+    }
 
 </style>
