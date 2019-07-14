ab9a491f6371ed4f65ada0fcc18cf299c24bb1da
svend <sven.depickere@student.howest.be>
2019-07-14 13:31:51 +0200

    Create PossibleLanguage.js

---
+++ app/assets/js/Vue/PossibleLanguage.js
@@ -0,0 +1,29 @@
+/**
+ Created by svend on 14/07/2019.
+ **/
+
+module.exports = (function () {
+
+    let languageAvailable = [
+        {
+            nameNative: 'English',
+            name: 'English',
+            abbr: 'EN'
+        },
+        {
+            nameNative: '中文',
+            name: 'Traditional Chinese',
+            abbr: 'ZH'
+        },
+        {
+            nameNative: '한국어',
+            name: 'Korean',
+            abbr: 'KO'
+        }
+    ];
+
+    return {
+        languageAvailable
+    }
+
+})();
