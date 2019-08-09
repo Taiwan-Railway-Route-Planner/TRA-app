d37237c01626817b4162fd7cd262b5ddd586188a
svend <sven.depickere@student.howest.be>
2019-08-09 14:40:12 +0200

    Update urlModule.js

--- app/assets/js/request/urlModule.js
+++ app/assets/js/request/urlModule.js
@@ -7,10 +7,14 @@ module.exports = (function () {
     const HOME_IP = "depickeresven.github.io";
     const BASE_URL = "https://" + HOME_IP + "/TRATimetableData/";
 
+    const EXTERNALIP = "traapp.tk";
+    const BACKEND = "https://" + EXTERNALIP + "/api/";
+
     const url = {
         schedules: BASE_URL + "Schedules/DATE.json",
         lines: BASE_URL + "Lines/LINENAME.json",
         station: BASE_URL + "stationInfo.json",
+        schedulesDay: BACKEND + "route"
     };
 
     return {
