61ad152b17ddb7fe092ed9694517c67d3eeb18d9
svend <sven.depickere@student.howest.be>
2019-08-09 14:40:05 +0200

    Create PostFormat.js

---
+++ app/assets/js/request/RequestFormat/PostFormat/PostFormat.js
@@ -0,0 +1,32 @@
+/**
+ Created by svend on 9/08/2019.
+ **/
+
+module.exports = (function () {
+
+    let fetchRequestForGettingData = function (fetchData) {
+        console.log(fetchData.Url);
+        console.log((fetchData.body));
+        console.log(JSON.stringify(fetchData.body));
+        return fetch(fetchData.getSpecifiedElement ? fetchData.Url + fetchData.specifiedElement : fetchData.Url, {
+            method: fetchData.method,
+            body: JSON.stringify(fetchData.body)
+        }).then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    return {error: true, msg: '', data: response};
+                }
+            }).then(json => {
+                return {error: false, msg: 'ok', data: json};
+            }).catch(function (error) {
+                console.log('Request failed ', error);
+                return {error: true, msg: '', data: error};
+            })
+    };
+
+    return {
+        fetchRequestForGettingData
+    }
+
+})();
