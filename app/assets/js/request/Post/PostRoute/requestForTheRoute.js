a50d48c68158ed9b1542dc3b37034d6d7d419e31
svend <sven.depickere@student.howest.be>
2019-08-09 14:40:02 +0200

    Create requestForTheRoute.js

---
+++ app/assets/js/request/Post/PostRoute/requestForTheRoute.js
@@ -0,0 +1,43 @@
+/**
+ Created by svend on 9/08/2019.
+ **/
+
+const postRequest = require('../../RequestFormat/PostFormat/PostFormat.js');
+const urlModule = require('../../urlModule');
+const errorMessages = require('../../errorMessage');
+
+module.exports =  (function () {
+
+    const getTheInformation = async function (_self) {
+        const response = await postRequest.fetchRequestForGettingData({
+            Url: urlModule.url.schedulesDay,
+            getSpecifiedElement: false,
+            method: "POST",
+            body: {
+                departure: {
+                    details : _self.$props.routeDetails.departure.details
+                },
+                arrival: {
+                    details: _self.$props.routeDetails.arrival.details
+                },
+                time:{
+                    date:  _self.$props.routeDetails.time.date,
+                    time:  _self.$props.routeDetails.time.time
+                }
+            }
+        });
+        if (response.error) {
+            // if (_self.$store.state.language === "ZH"){
+            //     response.msg = errorMessages.messages.zh.stations;
+            // } else {
+            response.msg = errorMessages.messages.eng.route;
+            // }
+        }
+        return response;
+    };
+
+    return {
+        getTheInformation
+    }
+
+})();
