c833bae472acec7f365ae88252d5d6226535209c
svend <sven.depickere@student.howest.be>
2019-08-11 15:28:11 +0200

    Update InternetConnection.js

--- app/assets/js/Vue/InternetConnection.js
+++ app/assets/js/Vue/InternetConnection.js
@@ -3,15 +3,22 @@
  **/
 
 const connectivityModule = require("tns-core-modules/connectivity");
+// const FeedbackPlugin = require("nativescript-feedback");
+// const feedback = new FeedbackPlugin.Feedback();
 
 module.exports = (function () {
 
-    function checkInternetConnection() {
+    function checkInternetConnection(_self) {
         const myConnectionType = connectivityModule.getConnectionType();
         switch (myConnectionType) {
             case connectivityModule.connectionType.none:
                 // Denotes no Internet connection.
                 console.log("No internet connection");
+                // console.log(feedback);
+                // console.log(feedback.error);
+                // feedback.error({
+                //     title: "No internet connection!",
+                // });
                 return false;
             case connectivityModule.connectionType.wifi:
             case connectivityModule.connectionType.mobile:
