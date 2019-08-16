790c272bd5e6fd81965699ecd166ab770812a80d
svend <sven.depickere@student.howest.be>
2019-08-16 13:14:51 +0200

    Update getStationDetails.js

--- app/assets/js/Vue/Select/getStationDetails.js
+++ app/assets/js/Vue/Select/getStationDetails.js
@@ -7,14 +7,20 @@ const requestHandler = require("../../request/requestHandler");
 module.exports = (function () {
 
     async function getAllPossibleStations(_self) {
-        if (_self.$store.state.stations === null || _self.$store.state.stations === undefined){
+        // if (_self.$store.state.stations === null || _self.$store.state.stations === undefined){
+            await getSearchFileToSearchStations();
             const stations = await requestHandler.request.stations(_self);
             _self.$store.commit('updateStation', stations.data);
             _self.data.stationInfo = stations.data;
-        } else {
-            _self.data.stationInfo = _self.$store.state.stations;
-        }
+        // } else {
+        //     _self.data.stationInfo = _self.$store.state.stations;
+        // }
         _self.filteredStations = _self.data.stationInfo.stations;
+    }
+
+    async function getSearchFileToSearchStations(_self){
+        const searchFile = await requestHandler.request.searchFileForStations(_self);
+        _self.$store.commit('updateSearchFile', searchFile.data);
     }
 
     async function getAllRoutesOfACertainDay(_self) {
