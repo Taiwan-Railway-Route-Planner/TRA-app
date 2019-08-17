3481fcac17b45e75e34f18a92a7e62778ad39360
svend <sven.depickere@student.howest.be>
2019-08-17 12:34:03 +0200

    Create LoadingModal.vue

---
+++ app/components/modals/LoadingModal.vue
@@ -0,0 +1,21 @@
+<template>
+    <StackLayout class="modal">
+        <Gif src="~/assets/images/train.gif"></Gif>
+    </StackLayout>
+</template>
+
+<script>
+    export default {
+        name: "LoadingModal"
+    }
+</script>
+
+<style scoped>
+
+    .modal {
+        backgroud-color: transparent;
+        height: 190;
+        width: 250;
+    }
+
+</style>
