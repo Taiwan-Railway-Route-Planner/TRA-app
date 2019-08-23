3afa49091a634c2765f0333c59e0b5a19774ac47
svend <sven.depickere@student.howest.be>
2019-08-23 12:47:03 +0200

    Update handleNavigationForResults.js

--- app/assets/js/Vue/Select/handleNavigationForResults.js
+++ app/assets/js/Vue/Select/handleNavigationForResults.js
@@ -107,7 +107,8 @@ module.exports = (function () {
     }
 
     return {
-        checkIfTheValuesAreCorrectBeforeWeCanStartSearchingAfterPossibleRoute
+        checkIfTheValuesAreCorrectBeforeWeCanStartSearchingAfterPossibleRoute,
+        internetError
     }
 
 })();
