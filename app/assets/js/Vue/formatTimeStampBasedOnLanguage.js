6cc956b0e17864f37aa2620b1db6df1d7a3b98de
svend <sven.depickere@student.howest.be>
2019-08-28 15:12:48 +0200

    Update formatTimeStampBasedOnLanguage.js from Dutch language

--- app/assets/js/Vue/formatTimeStampBasedOnLanguage.js
+++ app/assets/js/Vue/formatTimeStampBasedOnLanguage.js
@@ -14,6 +14,8 @@ export default (function () {
                 return moment(timeMoment).format('llll').replace(/\d\d:\d\d/i, '', '').replace('一', '');
             case "KO":
                 return moment(timeMoment).format('llll').replace(/\d\d:\d\d/i, '', '').replace('오전', '').replace('오후', '');
+            case "NL":
+                return moment(timeMoment).format('llll').replace(/\d\d:\d\d/i, '', '');
         }
     }
 
@@ -25,6 +27,8 @@ export default (function () {
                 return moment(timeMoment).format('llll').replace(/\d\d:\d\d/i, '', '').replace('一', '');
             case "KO":
                 return moment(timeMoment).format('lll').replace(/\d\d:\d\d/i, '', '').replace('오전', '').replace('오후', '');
+            case "NL":
+                return moment(timeMoment).format('llll').replace(/\d\d:\d\d/i, '', '');
         }
     }
 
@@ -36,6 +40,8 @@ export default (function () {
                 return moment().format('llll');
             case "KO":
                 return moment().format('lll');
+            case "NL":
+                return moment().format('llll')
         }
     }
 
