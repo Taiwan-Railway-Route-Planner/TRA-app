a863bd022e9fb93ede6ae359ee972568b027aa6d
svend <sven.depickere@student.howest.be>
2019-08-29 11:15:04 +0200

    Add language options for sharing

---
+++ app/assets/js/Vue/Route/TransfersOrNot/language.js
@@ -0,0 +1,27 @@
+/**
+ Created by svend on 29/08/2019.
+ **/
+
+module.exports = (function () {
+
+    let language = {
+        en: {
+            sentence: "I'm taking the train from POINTA at TIMEA and I will arrive in POINTB at TIMEB. \n Download the app https://www.traapp.tk "
+        },
+        zh: {
+            sentence: "Ik vertrek van POINTA om TIMEA en kom aan in POINTB om TIMEB. \n Download de app https://www.traapp.tk "
+        },
+        ko: {
+            sentence: "Ik vertrek van POINTA om TIMEA en kom aan in POINTB om TIMEB. \n Download de app https://www.traapp.tk "
+        },
+        nl: {
+            sentence: "Ik neem de trein van POINTA om TIMEA en kom aan in POINTB om TIMEB. \n Download de app https://www.traapp.tk "
+        }
+    };
+
+
+    return {
+        language
+    }
+
+})();
