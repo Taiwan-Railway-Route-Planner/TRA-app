88506b3ed96b98c4068b97f8c3ee1e2b67e4ce17
svend <sven.depickere@student.howest.be>
2019-08-29 11:18:48 +0200

    Update language.js

--- app/assets/js/Vue/Route/TransfersOrNot/language.js
+++ app/assets/js/Vue/Route/TransfersOrNot/language.js
@@ -6,16 +6,16 @@ module.exports = (function () {
 
     let language = {
         en: {
-            sentence: "I'm taking the train from POINTA at TIMEA and I will arrive in POINTB at TIMEB. \n Download the app https://www.traapp.tk "
+            sentence: "I'm taking the train from POINTA at TIMEA and I will arrive in POINTB at TIMEB. \nDownload the app https://www.traapp.tk "
         },
         zh: {
-            sentence: "Ik vertrek van POINTA om TIMEA en kom aan in POINTB om TIMEB. \n Download de app https://www.traapp.tk "
+            sentence: "Ik vertrek van POINTA om TIMEA en kom aan in POINTB om TIMEB. \nDownload de app https://www.traapp.tk "
         },
         ko: {
-            sentence: "Ik vertrek van POINTA om TIMEA en kom aan in POINTB om TIMEB. \n Download de app https://www.traapp.tk "
+            sentence: "Ik vertrek van POINTA om TIMEA en kom aan in POINTB om TIMEB. \nDownload de app https://www.traapp.tk "
         },
         nl: {
-            sentence: "Ik neem de trein van POINTA om TIMEA en kom aan in POINTB om TIMEB. \n Download de app https://www.traapp.tk "
+            sentence: "Ik neem de trein van POINTA om TIMEA en kom aan in POINTB om TIMEB. \nDownload de app https://www.traapp.tk "
         }
     };
 
