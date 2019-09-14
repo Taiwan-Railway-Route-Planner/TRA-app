79a23645f74a35f8edb9f57a136d247a98b90567
svend <sven.depickere@student.howest.be>
2019-09-14 17:17:18 +0200

    Create handle.js

---
+++ app/assets/js/Vue/BootUp/handle.js
@@ -0,0 +1,40 @@
+/**
+ Created by svend on 14/09/2019.
+ **/
+
+const PossibleLanguage = require('../PossibleLanguage');
+const language = require("./language");
+
+export default (function () {
+
+
+    const handle = function (_self) {
+        _self.languages = PossibleLanguage.languageAvailable;
+    };
+
+    const loadSelectedLanguage = function (_self, abbr) {
+        // TODO if add new language
+        switch (abbr) {
+            case "EN":
+                _self.data = language.language.en;
+                break;
+            case "ZH":
+                _self.data = language.language.zh;
+                break;
+            case "KO":
+                _self.data = language.language.ko;
+                break;
+            case "NL":
+                _self.data = language.language.nl;
+                break;
+        }
+        _self.saveAbbr = abbr;
+        _self.selected = true;
+    };
+
+    return {
+        handle,
+        loadSelectedLanguage
+    }
+
+})();
