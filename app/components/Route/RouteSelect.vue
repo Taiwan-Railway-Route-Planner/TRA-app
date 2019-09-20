becacb422a8ec140f8b6bbfd873cd9aaae62ca51
svend <sven.depickere@student.howest.be>
2019-09-20 14:02:29 +0200

    Add back navigation

--- app/components/Route/RouteSelect.vue
+++ app/components/Route/RouteSelect.vue
@@ -3,17 +3,20 @@
         <DockLayout>
             <FlexboxLayout dock="top" class="dock-top">
                 <FlexboxLayout class="top">
-                    <FlexboxLayout class="top-title">
-                        <Label :text="data.top.first + ':'"></Label>
-                        <Label :text="data.top.second + ':'"></Label>
-                    </FlexboxLayout>
-                    <FlexboxLayout v-if="$store.state.language === 'ZH'" class="top-data">
-                        <Label :text="$props.routeDetails.departure.details.站名"></Label>
-                        <Label :text="$props.routeDetails.arrival.details.站名"></Label>
-                    </FlexboxLayout>
-                    <FlexboxLayout v-else class="top-data">
-                        <Label :text="$props.routeDetails.departure.details.eng站名"></Label>
-                        <Label :text="$props.routeDetails.arrival.details.eng站名"></Label>
+                    <FlexboxLayout class="navDetails" @tap="navigateBackVue">
+                        <Label class="fas" :text="'\uf060' | unescape"></Label>
+                        <FlexboxLayout class="top-title">
+                            <Label :text="data.top.first + ':'"></Label>
+                            <Label :text="data.top.second + ':'"></Label>
+                        </FlexboxLayout>
+                        <FlexboxLayout v-if="$store.state.language === 'ZH'" class="top-data">
+                            <Label :text="$props.routeDetails.departure.details.站名"></Label>
+                            <Label :text="$props.routeDetails.arrival.details.站名"></Label>
+                        </FlexboxLayout>
+                        <FlexboxLayout v-else class="top-data">
+                            <Label :text="$props.routeDetails.departure.details.eng站名"></Label>
+                            <Label :text="$props.routeDetails.arrival.details.eng站名"></Label>
+                        </FlexboxLayout>
                     </FlexboxLayout>
                 </FlexboxLayout>
             </FlexboxLayout>
@@ -22,7 +25,7 @@
                     <Label class="fas" :text="'\uf3c5' | unescape"></Label>
                 </FlexboxLayout>
                 <FlexboxLayout class="menu-choose" @tap="navToStar">
-<!--                    <Label class="fas" :text="'\uf005' | unescape"></Label>-->
+                    <!--                    <Label class="fas" :text="'\uf005' | unescape"></Label>-->
                 </FlexboxLayout>
                 <FlexboxLayout class="menu-choose" @tap="navToSet">
                     <Label class="fas" :text="'\uf509' | unescape"></Label>
@@ -124,6 +127,9 @@
             navToSet: function () {
                 this.$goto("Settings");
             },
+            navigateBackVue: function () {
+                this.$navigateBack();
+            },
             onItemTap: function (event) {
                 if (this.$props.timeTable.multi) {
                     this.$goto('MultiRouteDetails', {
@@ -172,6 +178,16 @@
         flex-direction: column;
         margin-bottom: 1%;
         margin-right: 10%;
+    }
+
+    .top .navDetails {
+        flex-direction: row;
+        align-items: center;
+    }
+
+    .top .fas{
+        font-size: 30;
+        margin-right: 6%;
     }
 
     .top .top-data {
