5674a4be02918be8da1a70ede3ae8ec6aa29ddea
svend <sven.depickere@student.howest.be>
2019-09-25 11:01:32 +0200

    Init new language file

---
+++ app/assets/js/Vue/Support/language.js
@@ -0,0 +1,39 @@
+/**
+ Created by svend on 25/09/2019.
+ **/
+
+module.exports = (function () {
+
+    let language = {
+        en: {
+            top: {
+                title: ""
+            }
+        },
+        zh: {
+            top: {
+                title: ""
+            }
+        },
+        ko: {
+            top: {
+                title: ""
+            }
+        },
+        nl: {
+            top: {
+                title: ""
+            }
+        },
+        es: {
+            top: {
+                title: ""
+            }
+        }
+    };
+
+    return {
+        language
+    }
+
+})();
