cde38e7c8cec9b5ccc89c7b06d999eb26fa1759f
svend <sven.depickere@student.howest.be>
2019-09-25 11:01:48 +0200

    Init Support Vue

--- app/components/Support/Support.vue
+++ app/components/Support/Support.vue
@@ -1,10 +1,42 @@
 <template>
-    
+    <Page actionBarHidden="true">
+        <DockLayout>
+            <FlexboxLayout dock="top" class="dock-top">
+                <FlexboxLayout class="title">
+                    <Label class="" :text="data.top.title"></Label>
+                </FlexboxLayout>
+            </FlexboxLayout>
+            <FlexboxLayout dock="bottom" class="bottom-menu">
+                <FlexboxLayout class="menu-choose" @tap="navToStart">
+                    <Label class="fas" :text="'\uf3c5' | unescape"></Label>
+                </FlexboxLayout>
+                <FlexboxLayout class="menu-choose" @tap="navToStar">
+                    <!--<Label class="fas" :text="'\uf005' | unescape"></Label>-->
+                </FlexboxLayout>
+                <FlexboxLayout class="menu-choose">
+                    <Label class="fas" :text="'\uf509' | unescape"></Label>
+                </FlexboxLayout>
+            </FlexboxLayout>
+        </DockLayout>
+    </Page>
 </template>
 
 <script>
+    
+    import handle from "../../assets/js/Vue/Support/handle"
+    
     export default {
-        name: "Support"
+        created: function () {
+            handle.handleSetUpOfVue(this);
+        },
+        data () {
+            return {
+                data: null
+            }
+        },
+        methods: {
+            
+        }
     }
 </script>
 
