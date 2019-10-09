fbd6735ddd8808620b3303a51a0e26a11e3e9554
svend <sven.depickere@student.howest.be>
2019-10-09 12:52:56 +0200

    creating scalinglabel

--- app/main.js
+++ app/main.js
@@ -9,12 +9,13 @@ import Settings from "./components/Settings/Settings"
 import Support from "./components/Support/Support"
 import store from './store';
 
-import { Gif } from "nativescript-gif";
+import {Gif} from "nativescript-gif";
 
 import VueDevtools from 'nativescript-vue-devtools'
 
-if(TNS_ENV !== 'production') {
-  Vue.use(VueDevtools)
+
+if (TNS_ENV !== 'production') {
+    Vue.use(VueDevtools)
 }
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production');
@@ -23,24 +24,42 @@ Vue.filter('unescape', v => unescape(v));
 
 Vue.registerElement("Gif", () => Gif);
 
+import {Label} from "tns-core-modules/ui/label";
+
+class ScalingLabel extends Label {
+    initNativeView() {
+        super.initNativeView();
+        if (this.android) {
+            if (androidx)
+                androidx.core.widget.TextViewCompat.setAutoSizeTextTypeUniformWithConfiguration(this.nativeViewProtected, 10, 100, 1, android.util.TypedValue.COMPLEX_UNIT_SP);
+            else {
+                android.support.v4.widget.TextViewCompat.setAutoSizeTextTypeUniformWithConfiguration(this.nativeViewProtected, 10, 100, 1, android.util.TypedValue.COMPLEX_UNIT_SP);
+            }
+            // androidx.core.widget.TextViewCompat.setAutoSizeTextTypeUniformWithConfiguration(this.nativeViewProtected, 10, 100, 1, android.util.TypedValue.COMPLEX_UNIT_SP);
+        }
+    }
+}
+
+Vue.registerElement('ScalingLabel', () => ScalingLabel);
+
 const router = {
-  Bootup: Bootup,
-  Select: Select,
-  Route: Route,
-  RouteDetails: RouteDetails,
-  MultiRouteDetails: MultiRouteDetails,
-  RouteStopDetails: RouteStopDetails,
-  Settings: Settings,
-  Support: Support
+    Bootup: Bootup,
+    Select: Select,
+    Route: Route,
+    RouteDetails: RouteDetails,
+    MultiRouteDetails: MultiRouteDetails,
+    RouteStopDetails: RouteStopDetails,
+    Settings: Settings,
+    Support: Support
 };
 
 Vue.prototype.$router = router;
 
 Vue.prototype.$goto = function (to, props = null) {
-  this.$navigateTo(this.$router[to], props)
+    this.$navigateTo(this.$router[to], props)
 };
 
 new Vue({
-  store,
-  render: h => h('frame', [h(store.state.language !== null ? Select : Bootup)])
+    store,
+    render: h => h('frame', [h(store.state.language !== null ? Select : Bootup)])
 }).$start();
