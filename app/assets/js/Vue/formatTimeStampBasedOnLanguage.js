73fc2bbca7e4c1fd239989e0efc69553a0afa8d7
svend <sven.depickere@student.howest.be>
2019-10-11 13:27:20 +0200

    load correct timestamp

--- app/assets/js/Vue/formatTimeStampBasedOnLanguage.js
+++ app/assets/js/Vue/formatTimeStampBasedOnLanguage.js
@@ -23,6 +23,8 @@ export default (function () {
                 return moment(timeMoment).format('llll').replace(/\d\d:\d\d/i, '', '');
             case "FR":
                 return moment(timeMoment).format('llll').replace(/\d\d:\d\d/i, '', '');
+            case "AR":
+                return moment(timeMoment).format('llll').replace(/\d\d:\d\d/i, '', '');
         }
     }
 
@@ -42,6 +44,8 @@ export default (function () {
                 return moment(timeMoment).format('llll').replace(/\d\d:\d\d/i, '', '');
             case "FR":
                 return moment(timeMoment).format('llll').replace(/\d\d:\d\d/i, '', '');
+            case "AR":
+                return moment(timeMoment).format('llll').replace(/\d\d:\d\d/i, '', '');
         }
     }
 
@@ -60,6 +64,8 @@ export default (function () {
             case "DE":
                 return moment().format('llll');
             case "FR":
+                return moment().format('llll');
+            case "AR":
                 return moment().format('llll');
         }
     }
