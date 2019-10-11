f31b5afb452518a62a024c673f91af702ee0adc6
svend <sven.depickere@student.howest.be>
2019-10-11 13:58:20 +0200

    fix typo

--- app/assets/js/Vue/Settings/language.js
+++ app/assets/js/Vue/Settings/language.js
@@ -150,7 +150,7 @@ module.exports = (function () {
                 }
             }
         },
-        RU: {
+        ru: {
             "top": {
                 "title": "Настройки"
             },
