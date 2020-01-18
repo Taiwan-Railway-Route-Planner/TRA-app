fcb50e6af2654d88839e36542c0cd50068462495
svend <sven.depickere@student.howest.be>
2020-01-18 13:46:44 +0100

    clean up handle timeModal

--- app/assets/js/Vue/Modal/timeModal/handle.js
+++ app/assets/js/Vue/Modal/timeModal/handle.js
@@ -6,22 +6,6 @@ const moment = require('moment');
 
 export default (function () {
 
-    const handle = function (_self) {
-        setTime(_self);
-        setDateDetails(_self);
-    };
-
-    function setTime(_self) {
-        _self.timeModal.selectedTime = moment().toDate();
-    }
-
-    function setDateDetails(_self) {
-        moment().locale(_self.$props.time.local);
-        _self.selectedDate = moment().format('L');
-        _self.$props.time.modal.center.date.today = moment().format();
-        _self.$props.time.modal.center.date.actual = _self.$props.formatTimeStampBasedOnLanguage.formatTimeStampForModel(_self);
-    }
-
     const discard = function (_self) {
 
     };
@@ -30,14 +14,13 @@ export default (function () {
         _self.time.date.show = _self.$props.formatTimeStampBasedOnLanguage.formatTimeStampForShowingSelect(_self, _self.$props.time.modal.center.date.today);
         _self.time.date.real = moment(_self.$props.time.modal.center.date.today).locale('en').format('YYYYMMDD');
         if (_self.$store.state.language === "KO"){
-            _self.time.time = moment(_self.timeModal.selectedTime).locale('en-SG').format('LT');
+            _self.time.time = moment(_self.timeWeSelected).locale('en-SG').format('LT');
         } else {
-            _self.time.time = moment(_self.timeModal.selectedTime).format('LT');
+            _self.time.time = moment(_self.timeWeSelected).format('LT');
         }
     };
 
     return {
-        handle,
         discard,
         save
     }
