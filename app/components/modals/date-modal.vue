b28abf1ff59797cd694053c19daae3aedc42cf8d
Depickere Sven <sven.depickere@student.howest.be>
2020-02-22 12:24:45 +0100

    Clean up scss

--- app/components/modals/date-modal.vue
+++ app/components/modals/date-modal.vue
@@ -1,14 +1,6 @@
 <template>
     <Flexboxlayout :class="[smallerLabels, 'center']">
-<!--        <FlexboxLayout dock="bottom" class="confirmOrDiscard">-->
-<!--            <Label class="btn discardBtn" @tap="discard" :text="$props.time.modal.bottom.leftBtn"/>-->
-<!--            <Label class="btn confirmBtn" @tap="confirm" :text="$props.time.modal.bottom.rightBtn"/>-->
-<!--        </FlexboxLayout>-->
-<!--        <FlexboxLayout dock="center" class="center">-->
-<!--        <FlexboxLayout class="center">-->
             <DatePicker :minDate="minDate" :maxDate="maxDate"/>
-<!--        </FlexboxLayout>-->
-<!--        </FlexboxLayout>-->
     </Flexboxlayout>
 </template>
 
@@ -61,19 +53,13 @@
     @import "../../theme/generalStyles.scss";
     @import "../../theme/_variables.scss";
 
-    .modal {
-        background-color: $white;
-        text-align: left;
-        color: $dark;
-    }
-
     .center{
         justify-content: center;
     }
 
     /****** GENERATE FROM COMPUTED smallerLabels() ******/
 
-    .smallerLabels.modal Label{
+    .smallerLabels.center Label{
         font-size: 14;
     }
 
--- app/components/modals/time-modal.vue
+++ app/components/modals/time-modal.vue
@@ -62,19 +62,6 @@
     @import "../../theme/generalStyles.scss";
     @import "../../theme/_variables.scss";
 
-    /*.modal {
-        background-color: $white;
-        text-align: left;
-        color: $dark;
-    }*/
-
-    /****** GENERATE FROM COMPUTED smallerLabels() ******/
-
-    /*.smallerLabels.modal Label,*/
-    /*.smallerLabels.modal .departureOrArrival .smaller-departureOrArrival .topLabel {*/
-    /*    font-size: 14;*/
-    /*}*/
-
     .timeSettings {
         flex-direction: column;
         width: 100%;
@@ -109,100 +96,5 @@
             }
         }
     }
-
-    /*.smallerLabels.timeSettings .timeSelect .now Button {*/
-    /*    font-size: 12;*/
-    /*}*/
-
-    /*.timeSettings {
-        flex-direction: column;
-        width: 100%;
-        height: 60%;
-    }*/
-
-    /*.timeSettings .timeSelect {
-        flex-direction: row;
-        align-self: center;
-        margin-bottom: -20;
-        padding-top: -20;
-    }
-
-    .timeSettings .timeSelect TimePicker {
-        width: 80%;
-    }
-
-    .timeSettings .timeSelect .now {
-        flex-direction: column;
-        align-self: center;
-        width: 20%;
-    }
-
-    .timeSettings .timeSelect .now Button {
-        !*iOS*!
-        width: 80%;
-        margin-left: auto;
-        margin-right: auto;
-    }*/
-
-    /*.timeSettings .dateTimeSelect {
-        flex-direction: row;
-        width: 100%;
-        align-items: center;
-        margin-left: 10;
-    }
-
-    .timeSettings .dateTimeSelect .dateTimeNav {
-        flex-direction: row;
-        justify-content: space-between;
-        width: 80%;
-    }
-
-    .timeSettings .dateTimeSelect .dateTimeNav .fas {
-        color: $primary;
-    }
-
-    .timeSettings .dateTimeSelect .dateTimeNav .timeStamp {
-        flex-direction: column;
-        align-self: center;
-    }
-
-    .timeSettings .dateTimeSelect .calenderView {
-        !*Android*!
-        !*width: 20%;*!
-        !*iOS*!
-        width: 30%;
-        height: 100px;
-    }
-
-    .timeSettings .dateTimeSelect .calenderView .far {
-        font-size: 20;
-    }
-
-    .departureOrArrival {
-        flex-direction: row;
-        width: 100%;
-        justify-content: center;
-    }
-
-    .departureOrArrival .smaller-departureOrArrival {
-        width: 80%;
-        border-bottom-width: 3px;
-        border-bottom-color: $white;
-        border-bottom-style: solid;
-        text-align: center;
-    }
-
-    .departureOrArrival .smaller-departureOrArrival .topLabel {
-        width: 50%;
-        font-size: 18;
-        padding-top: 15;
-        padding-bottom: 15;
-    }
-
-    .departureOrArrival .smaller-departureOrArrival .active {
-        border-bottom-width: 6px;
-        border-bottom-color: $primary;
-        border-bottom-style: solid;
-    }*/
 
 </style>
